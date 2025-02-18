4560c623ba573647798e6d03067fd2969e0c4a0c
Adam <adam_josefsson@live.com>
2025-02-18 18:58:43 +0100

    chore: add clang-format

--- src/led.c
+++ src/led.c
@@ -1,25 +1,25 @@
 #include "led.h"
 #include "stm32l552.h"
 
-void led_init(void) 
+void led_init(void)
 {
     RCC_AHB2ENR |= RCC_GPIOBEN;
-    RCC_AHB2RSTR |= RCC_GPIOBRST; // Reset high
-    RCC_AHB2RSTR &= ~RCC_GPIOBRST; // Reset low
-    GPIOB_MODER &= ~GPIOB7_MODER; // Reset
-    GPIOB_MODER |= (0x01 << 14); // Set to General purpose output
-    GPIOB_OTYPER &= ~GPIOB7; // Set to type: Output push-pull (7: 0)
+    RCC_AHB2RSTR |= RCC_GPIOBRST;     // Reset high
+    RCC_AHB2RSTR &= ~RCC_GPIOBRST;    // Reset low
+    GPIOB_MODER &= ~GPIOB7_MODER;     // Reset
+    GPIOB_MODER |= (0x01 << 14);      // Set to General purpose output
+    GPIOB_OTYPER &= ~GPIOB7;          // Set to type: Output push-pull (7: 0)
     GPIOB_OSPEEDR &= ~GPIOB7_OSPEEDR; // Set to: Very low speed (15, 14: 00)
     GPIOB_PUPDR &= ~GPIOB7_PUPDR; // Set to: No pull up, pull down (15, 14: 00)
 }
 
-void led_set_state(bool ledOn) 
+void led_set_state(bool ledOn)
 {
-    if (ledOn) 
+    if(ledOn)
     {
         GPIOB_ODR |= GPIOB7;
     }
-    else 
+    else
     {
         GPIOB_ODR &= ~GPIOB7;
     }
--- src/main.c
+++ src/main.c
@@ -2,15 +2,15 @@
 
 #include <stdint.h>
 
-int main(void) 
+int main(void)
 {
     led_init();
 
     while(1)
     {
         led_set_state(true);
-        for (int i = 0; i < 500000; i++); // arbitrary delay
+        for(int i = 0; i < 500000; i++); // arbitrary delay
         led_set_state(false);
-        for (int i = 0; i < 500000; i++); // arbitrary delay
+        for(int i = 0; i < 500000; i++); // arbitrary delay
     }
 }
